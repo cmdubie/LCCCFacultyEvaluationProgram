68ee27427b918d1eab524f95844c0a5a54eb1441
cmd924 <chad.m.dubesa@sherwin.com>
2014-05-05 10:08:07 -0400

    Making things current so I can pull

---
+++ App_Data/dbo.COMMENT.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[COMMENT] (
+    [CommentID]       INT           IDENTITY (1, 1) NOT NULL,
+    [ClassNum]        INT           NOT NULL,
+    [Term]            INT           NOT NULL,
+    [StudentComments] VARCHAR (1024) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CommentID] ASC, [Term] ASC, [ClassNum] ASC)
+);
+
