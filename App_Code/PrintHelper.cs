fed1c92d6d1ae9754472ad2a233f312d018baaf9
cmdubie <cmdubie@live.com>
2014-05-13 00:07:22 -0400

    - all calls to  PrintWebControl point to method with parameter ArrayList

--- App_Code/PrintHelper.cs
+++ App_Code/PrintHelper.cs
@@ -21,29 +21,8 @@ public class PrintHelper
 
     public static void PrintWebControl(Control ctrl, string script)
     {
-        var stringWrite = new StringWriter();
-        var htmlWrite = new HtmlTextWriter(stringWrite);
-        var control = ctrl as WebControl;
-        if (control != null)
-        {
-            var w = new Unit(100, UnitType.Percentage); control.Width = w;
-        }
-        var pg = new Page {EnableEventValidation = false};
-        if (script != string.Empty)
-        {
-            pg.ClientScript.RegisterStartupScript(pg.GetType(), "PrintJavaScript", script);
-        }
-        var frm = new HtmlForm();
-        pg.Controls.Add(frm);
-        frm.Attributes.Add("runat", "server");
-        frm.Controls.Add(ctrl);
-        pg.DesignerInitialize();
-        pg.RenderControl(htmlWrite);
-        string strHTML = stringWrite.ToString();
-        HttpContext.Current.Response.Clear();
-        HttpContext.Current.Response.Write(strHTML);
-        HttpContext.Current.Response.Write("<script>window.print();</script>");
-        HttpContext.Current.Response.End();
+        var ctrlArray = new ArrayList {ctrl};
+        PrintWebControl(ctrlArray, string.Empty);
     }
 
     public static void PrintWebControl(ArrayList ctrlArray, string script)
